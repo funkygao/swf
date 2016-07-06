507f637dfbc0db2ebdccf91a0b9cdfb4175dcb29
funkygao <funky.gao@gmail.com>
2016-07-06 21:56:53 +0800

    a Server has several Service's

--- cmd/swfd/server/server.go
+++ cmd/swfd/server/server.go
@@ -3,21 +3,34 @@ package server
 import (
 	"net/http"
 
+	"github.com/funkygao/gafka/ctx"
+	"github.com/funkygao/gafka/zk"
 	log "github.com/funkygao/log4go"
+	"github.com/funkygao/swf/services"
 )
 
 // Server is the SimpleWorkFlow server engine.
 type Server struct {
 	apiServer *apiServer
+	services  []services.Service
+
+	zkzone *zk.ZkZone
 
 	shutdownChan chan struct{}
+}
+
+func init() {
+	ctx.LoadFromHome()
 }
 
 func New() *Server {
 	this := &Server{
 		apiServer:    newApiServer(),
+		services:     make([]services.Service, 0),
+		zkzone:       zk.NewZkZone(zk.DefaultConfig(Options.Zone, ctx.ZoneZkAddrs(Options.Zone))),
 		shutdownChan: make(chan struct{}),
 	}
+
 	return this
 }
 
@@ -32,8 +45,18 @@ func (this *Server) setupApis() {
 
 }
 
+func (this *Server) setupServices() {
+
+}
+
+func (this *Server) addService(svc services.Service) {
+	this.services = append(this.services, svc)
+}
+
 func (this *Server) start() error {
 	this.setupApis()
+	this.setupServices()
+
 	if err := this.apiServer.start(); err != nil {
 		return err
 	}
@@ -41,6 +64,12 @@ func (this *Server) start() error {
 	log.Info("engine ready")
 
 	return nil
+}
+
+func (this *Server) stop() {
+	for _, svc := range this.services {
+		svc.Stop()
+	}
 }
 
 func (this *Server) ServeForever() {
